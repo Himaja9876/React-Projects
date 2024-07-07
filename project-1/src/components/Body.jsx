4dbb5200c44bc647cbc334374a4fe05f599a148e
Himaja9876 <himajachinnam8@gmail.com>
2024-07-07 18:12:07 -0400

    updated with the changes

---
+++ project-1/src/components/Body.jsx
@@ -0,0 +1,36 @@
+
+const Body = () => {
+  return (
+    <div className="body">
+     <div className="text-box">
+        <div className="heading-text">
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+        </div>
+        
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="textbox-buttons">
+          <button>Shop Now</button>
+          <button>Category</button>
+        </div>
+        <div className="sites">
+          <p>Also Available On</p>
+          <div className="site-symbols">
+            <img src="Images/flipkart.png" alt="flipkart symbol"/>
+            <img src="Images/amazon.png" alt="amazon symbol" />
+          </div>
+        </div>  
+     </div>
+     
+     <div>
+        <img src="Images/shoe_image.png" alt="Nike Shoe"/>
+      </div> 
+               
+    </div>
+  )
+}
+
+export default Body
---
+++ project-1/src/components/Header.jsx
@@ -0,0 +1,26 @@
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="links">
+        
+        <div>
+          <img className="logo" src="/Images/brand_logo.png" alt="Nike Logo"/>
+        </div>       
+
+        <ul>
+          <li>Menu</li>
+          <li>Location</li>
+          <li>About</li>
+          <li>Location</li>
+        </ul>  
+        
+        <button>Login</button>      
+    </nav>
+    </div>
+    
+  
+  )
+}
+
+export default Header
